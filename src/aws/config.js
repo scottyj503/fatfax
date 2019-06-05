04fa9f32b1f3a893483ea7f11f491ab3678112b3
Scott Jones <scott@fatpackets.com>
2019-06-05 13:29:20 -0600

    WIP added cognito config

---
+++ src/aws/config.js
@@ -0,0 +1,8 @@
+export default {
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_2jfmhFOnv",
+    APP_CLIENT_ID: "66dn01i33edtb4ugmgt3e2kjki",
+    IDENTITY_POOL_ID: "us-east-1:6419fe8b-58d4-4ce0-94d4-3bc24720735d"
+  },
+};
--- src/client.js
+++ src/client.js
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import Amplify from 'aws-amplify';
 import store from './data/store';
 import Layout from './Layout';
+import config from './aws/config';
 import './client.css';
 
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID
+  },
+});
 const app = document.getElementById('app');
 
 ReactDom.render(
